638130c2371806553307875eb1099223ef97c5f4
dayo <www.dharmikdayani0959@gmail.com>
2022-08-16 00:38:25 +0530

    remove

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,7 +3,7 @@
 export const Blogs = [
   {
     id: 1,
-    name: "Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations this is s nice car👌",
+    name: "Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
     tags: ["react", "gsap", "styled-components"],
     date: "13 May, 2021",
     imgSrc:
